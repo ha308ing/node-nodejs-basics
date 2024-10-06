f3f0c2e63b70fca42309b43af53357022ec5bad2
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-06 13:12:18 +0400

    feat(worker_threads): main
    - spawn multiple threads
    - collect and output worker threads resutls

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from "node:worker_threads";
+import path from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPath = path.resolve(import.meta.dirname, "worker.js");
+    const resultsCount = 3;
+    const numberStart = 10;
+
+    let resultsCounter = 0;
+    const results = [];
+    for (let i = 0; i < resultsCount; i++) {
+        const worker = new Worker(workerPath, {
+            workerData: { n: i + numberStart },
+        });
+
+        worker.on("message", (result) => {
+            results[i] = { status: "resolved", data: result };
+        });
+
+        worker.on("error", (e) => {
+            results[i] = { status: "error", data: null };
+        });
+
+        worker.on("exit", () => {
+            resultsCounter++;
+            if (resultsCounter === resultsCount) {
+                console.log(results);
+            }
+        });
+    }
 };
 
 await performCalculations();
