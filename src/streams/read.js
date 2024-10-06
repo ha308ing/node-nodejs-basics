dec498f94ef14ae647c19d2bfd856676ba589986
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-06 13:07:23 +0400

    feat(streams): read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import path from "node:path";
+import { createReadStream } from "node:fs";
+import { Transform } from "node:stream";
+
 const read = async () => {
-    // Write your code here 
+    const file = "files/fileToRead.txt";
+    const filePath = path.resolve(import.meta.dirname, file);
+
+    const stream = createReadStream(filePath).setEncoding("utf-8");
+
+    stream
+        .pipe(
+            new Transform({
+                transform(chunk, _, callback) {
+                    chunk += "\n";
+                    callback(null, chunk);
+                },
+            })
+        )
+        .pipe(process.stdout);
 };
 
 await read();
