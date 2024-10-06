0887e87c72039f0c5fe215c68e7a985b3c78398d
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-06 13:00:25 +0400

    feat(fs): copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import path from "node:path";
+import fs from "node:fs";
+import { checkPaths, throwError, doOperation } from "./helpers/index.js";
+
 const copy = async () => {
-    // Write your code here 
+    const source = path.resolve(import.meta.dirname, "files");
+    const target = path.resolve(import.meta.dirname, "files_copy");
+
+    const [isSourceExist, isTargetExist] = await checkPaths(source, target);
+    const isError = !isSourceExist || isTargetExist;
+
+    if (isError) throwError();
+
+    doOperation(fs.promises.cp, source, target, { recursive: true });
 };
 
 await copy();
