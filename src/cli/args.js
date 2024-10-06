78ca6474361fe245353084d877e6f2a954cf6125
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-06 13:02:40 +0400

    feat(cli): args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const argsString = args.reduce((string, arg, index) => {
+        const isProperty = arg.startsWith("--");
+        const isLastArg = index == args.length - 1;
+        const argString = isProperty ? `${arg.substring(2)} is ` : arg;
+        string += `${argString}${!isLastArg && !isProperty ? ", " : ""}`;
+        return string;
+    }, "");
+    console.log(argsString);
 };
 
 parseArgs();
