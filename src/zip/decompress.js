77a61631b14cfe0bb212797695471d1a6eed78eb
unknown <75647984+ha308ing@users.noreply.github.com>
2024-10-06 13:08:46 +0400

    feat(zip): decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import path from "node:path";
+import fs from "node:fs";
+import zlib from "node:zlib";
+
+const __dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+    const inputPath = "files/archive.gz";
+    const outputPath = "files/fileToCompress.txt";
+    const input = path.resolve(__dirname, inputPath);
+    const output = path.resolve(__dirname, outputPath);
+
+    fs.createReadStream(input)
+        .pipe(zlib.createUnzip())
+        .pipe(fs.createWriteStream(output));
 };
 
 await decompress();
