05cf8b0a4a009647a2c5e8efdb8fe64e0e2ee46b
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-04-25 00:30:45 +0400

    refactor: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,32 @@
+const ARGS_START_INDEX = 2;
+const args = process.argv.slice(ARGS_START_INDEX);
+
+const propNameRegex = /^-{1,2}/;
+const outputStringSeparator = ", ";
+
 const parseArgs = () => {
-    const args = process.argv.slice(2);
-    const argsString = args.reduce((string, arg, index) => {
-        const isProperty = arg.startsWith("--");
-        const isLastArg = index == args.length - 1;
-        const argString = isProperty ? `${arg.substring(2)} is ` : arg;
-        string += `${argString}${!isLastArg && !isProperty ? ", " : ""}`;
-        return string;
-    }, "");
-    console.log(argsString);
+    let ouputString = "";
+    let isPropSet = false;
+
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+
+        if (propNameRegex.test(arg)) {
+            ouputString += isPropSet
+                ? ` is true`
+                : arg.replace(propNameRegex, "");
+        } else {
+            ouputString += ` is ${arg}`;
+
+            if (i < args.length - 1) {
+                ouputString += outputStringSeparator;
+            }
+        }
+
+        isPropSet = !isPropSet;
+    }
+
+    console.log(ouputString);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,16 +1,13 @@
+const envPrefix = "RSS_";
+const joinSeparator = "; ";
+
 const parseEnv = () => {
-    const entriesPrefix = "RSS_";
-    const entriesSep = "; ";
-
-    const vars = process.env;
-    const varEntries = Object.entries(vars);
-
-    const entries = varEntries.filter(([key]) => key.startsWith(entriesPrefix));
-    const entriesString = entries
+    const variablesString = Object.entries(process.env)
+        .filter(([key]) => key.startsWith(envPrefix))
         .map(([key, value]) => `${key}=${value}`)
-        .join(entriesSep);
+        .join(joinSeparator);
 
-    console.log(entriesString);
+    console.log(variablesString);
 };
 
 parseEnv();
