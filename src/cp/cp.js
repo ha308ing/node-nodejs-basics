0aa59bfb57ba05b98cdb32af8ede69be01c44fd8
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-04-25 00:35:19 +0400

    refactor: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,32 +1,10 @@
 import { fork } from "node:child_process";
-import path from "node:path";
-import util from "node:util";
+import { resolve } from "node:path";
+
+const script = resolve(import.meta.dirname, "./files/script.js");
 
 const spawnChildProcess = async (args) => {
-    const script = path.resolve(import.meta.dirname, "files/script.js");
-
-    const child = fork(script, args, {
-        stdio: "pipe",
-    });
-
-    process.stdin
-        .on("data", writePipe("send from master to child"))
-        .pipe(child.stdin);
-
-    child.stdout
-        .on("data", writePipe("send from child to master"))
-        .pipe(process.stdout);
+    fork(script, args);
 };
 
 spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
-
-function styleString(string) {
-    return util.styleText(["italic"], string);
-}
-
-function writePipe(string) {
-    return function (data) {
-        const stringStyled = styleString(`${string}: ${data.toString()}`);
-        process.stdout.write(stringStyled);
-    };
-}
